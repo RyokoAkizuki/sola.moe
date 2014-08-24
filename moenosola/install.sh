809e79ca16cf1bc9dc753696cdf3894c6c7405f8
Yukino Hayakawa <tennencoll@gmail.com>
2014-08-24 02:48:23 -0700

    install script with options

--- moenosola/install.sh
+++ moenosola/install.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-apt-get install php5-dev
+case "$1" in
+depend)
+    apt-get install php5-dev
 
-git clone https://github.com/CopernicaMarketingSoftware/PHP-CPP.git
-cd PHP-CPP
-make -j8
-make test
-make install
+    git clone https://github.com/CopernicaMarketingSoftware/PHP-CPP.git
+    cd PHP-CPP
+    make -j8
+    make test
+    make install
+    cd ..
+    ;;
 
-cd ..
-make -j8
-make install
-service php5-fpm restart
+install)
+    make clean
+    make -j8
+    make install
+    service php5-fpm restart
+    ;;
+
+*)
+    echo "Usage: install.sh {depend|install}"
+    exit 1
+    ;;
+esac
+
+exit 0
