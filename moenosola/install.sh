560f7125c5279e51c8afc2fe1a8257a51d4e9369
Yukino Hayakawa <tennencoll@gmail.com>
2014-08-24 02:24:29 -0700

    update install script

--- moenosola/install.sh
+++ moenosola/install.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-make clean
+apt-get install php5-dev
+
+git clone https://github.com/CopernicaMarketingSoftware/PHP-CPP.git
+cd PHP-CPP
+make -j8
+make test
+make install
+
+cd ..
 make -j8
 make install
 service php5-fpm restart
